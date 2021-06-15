eafa433819768ee24f90967444d221c4d6e4d12f
yama998877 <yamada.sss.369@gmail.com>
2021-06-15 10:39:14 +0900

    unless削除、条件追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,12 +2,13 @@
 i = 0
 while i <= 19
   i = i + 1
-  print i,"\n" unless i % 5 ==0 && i % 3 ==0 || i % 3 == 0 || i % 5 == 0
   if i % 5 ==0 && i % 3 ==0
     print "FizzBuzz\n"
   elsif i % 3 == 0
     print "Fizz\n"
   elsif i % 5 == 0
     print "Buzz\n"
+  elsif
+    print i,"\n"
   end
 end
