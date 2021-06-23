2f60b7f93199e3e68f85d3b323f4ddba8b0fceed
yama998877 <yamada.sss.369@gmail.com>
2021-06-23 23:16:53 +0900

    指摘箇所の修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,33 +3,30 @@
 require 'date'
 require 'optparse'
 #変数、現在の年、月、日
-year = Date.today.year
-month = Date.today.month
-day = Date.today.day
+today_year = Date.today.year
+today_month = Date.today.month
 #オプション -y -m
-options = ARGV.getopts("", "y:#{Date.today.year}", "m:#{Date.today.month}")
-year = options["y"].to_i
-month = options["m"].to_i
+options = ARGV.getopts("", "y:#{today_year}", "m:#{today_month}")
+today_year = options["y"].to_i
+today_month = options["m"].to_i
 #月末、月初
-last_day = Date.new(year,month,-1)
-first_day = Date.new(year,month,+1)
+last_day = Date.new(today_year,today_month,-1)
+first_day = Date.new(today_year,today_month,+1)
 #月 年
-month_year = Date.today.strftime("#{month}月 #{year}")
+month_year = Date.today.strftime("#{today_month}月 #{today_year}")
 #カレンダー
-puts month_year.center(24)
-puts " 日 月 火 水 木 金 土"
+puts month_year.center(20)
+puts "日 月 火 水 木 金 土"
 (first_day..last_day).each do |day|
   if first_day == day && day.saturday?
-    print "   " * first_day.wday+" ", "#{day.day}\n".rjust(3)," "
+    print "   " * first_day.wday, "#{day.day}".rjust(2),"\n"
   elsif first_day == day
-    print "   " * first_day.wday, "#{day.day}".rjust(3)," "
-  elsif day.saturday? && last_day == day
-    print "#{day.day}\n".rjust(3)," ","\n"
+    print "   " * first_day.wday, "#{day.day}".rjust(2)," "
   elsif day.saturday?
-    print "#{day.day}\n".rjust(3)," "
+    print "#{day.day}".rjust(2),"\n"
   elsif
     print "#{day.day}".rjust(2)," "
   elsif last_day == day
-    print "\n"
+    print "\n","\n"
   end
 end
