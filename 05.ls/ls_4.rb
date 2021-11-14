0b75cc442377517153eb6ddb9c5f178b459af474
yama998877 <yamada.sss.369@gmail.com>
2021-11-14 14:29:56 +0900

    前回のコミットの指摘箇所修正

--- 05.ls/ls_4.rb
+++ 05.ls/ls_4.rb
@@ -21,7 +21,7 @@ class Option
   end
 end
 
-def convert_into_permission(name)
+def convert_file_type_into_character(name)
   {
     'fifo' => 'p',
     'characterSpecial' => 'c',
@@ -29,7 +29,12 @@ def convert_into_permission(name)
     'blockSpecial' => 'b',
     'file' => '-',
     'link' => 'l',
-    'socket' => 's',
+    'socket' => 's'
+  }[name]
+end
+
+def convert_octal_number_into_character(name)
+  {
     '0' => '---',
     '1' => '--x',
     '2' => '-w-',
@@ -47,18 +52,8 @@ directories = Dir.glob('*', base: option.specified_directory)
 return if directories == []
 
 if option.has?(:l)
-  file_size = 0
-  directories.each do |size|
-    fs =
-      if option.specified_directory.nil?
-        File::Stat.new(size)
-      else
-        File::Stat.new(option.specified_directory + size)
-      end
-    file_size += fs.blocks
-  end
-  puts "total #{file_size}"
-
+  block_size = 0
+  file_info = []
   directories.each do |dir|
     fs =
       if option.specified_directory.nil?
@@ -66,40 +61,42 @@ if option.has?(:l)
       else
         File::Stat.new(option.specified_directory + dir)
       end
+    block_size += fs.blocks
     user_id    = fs.uid
     user_name  = Etc.getpwuid(user_id).name
     group_id   = fs.gid
     group_name = Etc.getgrgid(group_id).name
     file_mode = format('%06d', fs.mode.to_s(8))
-    print convert_into_permission(fs.ftype)
-    print convert_into_permission(file_mode[3])
-    print convert_into_permission(file_mode[4])
-    print convert_into_permission(file_mode[5])
-    print " #{fs.nlink.to_s.rjust(3)} #{user_name}  #{group_name} #{fs.size.to_s.rjust(6)} #{fs.mtime.mon.to_s.rjust(2)} #{fs.mtime.strftime('%e %R')} "
-    print "#{dir}\n"
+    file_info <<
+      convert_file_type_into_character(fs.ftype) +
+      convert_octal_number_into_character(file_mode[3]) +
+      convert_octal_number_into_character(file_mode[4]) +
+      convert_octal_number_into_character(file_mode[5]) +
+      " #{fs.nlink.to_s.rjust(3)} #{user_name}  #{group_name} #{fs.size.to_s.rjust(6)} #{fs.mtime.mon.to_s.rjust(2)} #{fs.mtime.strftime('%e %R')} #{dir}"
   end
-end
+  puts "total #{block_size}"
+  puts file_info
+else
 
-return if option.has?(:l)
+  max_size_directory = directories.max_by(&:length)
+  max_size = max_size_directory.size + 2
+  empty_space_added_directories = []
+  directories.each do |empty_space|
+    empty_space_added_directories << empty_space.ljust(max_size)
+  end
 
-max_size_directory = directories.max_by(&:length)
-max_size = max_size_directory.size + 2
-empty_space_added_directories = []
-directories.each do |empty_space|
-  empty_space_added_directories << empty_space.ljust(max_size)
-end
+  number_of_columns = 3.0
+  number_of_lines = (empty_space_added_directories.size / number_of_columns).ceil
 
-number_of_columns = 3.0
-number_of_lines = (empty_space_added_directories.size / number_of_columns).ceil
+  nil_padding = ((number_of_columns * number_of_lines) - empty_space_added_directories.size).to_i
+  nil_padding.times { empty_space_added_directories << nil }
 
-nil_padding = ((number_of_columns * number_of_lines) - empty_space_added_directories.size).to_i
-nil_padding.times { empty_space_added_directories << nil }
+  directory_lines = []
+  empty_space_added_directories.each_slice(number_of_lines) do |directory_columns|
+    directory_lines << directory_columns
+  end
 
-directory_lines = []
-empty_space_added_directories.each_slice(number_of_lines) do |directory_columns|
-  directory_lines << directory_columns
-end
-
-directory_lines.transpose.each do |file|
-  puts file.join
+  directory_lines.transpose.each do |file|
+    puts file.join
+  end
 end
